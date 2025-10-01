6e09cc62ede516dbfe3264fd8e8ab94de2ca0290
Muharrizk Abilaksana <ai.mo7g@gmail.com>
2025-10-01 17:12:41 +0700

    header hotfix

--- app.js
+++ app.js
@@ -14,12 +14,10 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) {
             $('header').css({
-                'box-shadow': '0 2px 20px rgba(0,0,0,0.08)',
                 'background': 'rgba(255, 255, 255, 0.98)'
             });
         } else {
             $('header').css({
-                'box-shadow': '0 2px 10px rgba(0,0,0,0.05)',
                 'background': 'rgba(255, 255, 255, 0.95)'
             });
         }
